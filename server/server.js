454c691cf552504e9f834cbc4f9efd75b1366c8e
Jesse Jafa <jesse.jafa@gmail.com>
2017-09-21 20:58:23 +0300

    Testing ip and headers of req object

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const requestIp = require('request-ip');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.use(requestIp.mw())
 
+
+app.get('/', (req, res) => {
+  console.log(req.headers);
+  var ip = req.clientIp;
+  console.log(ip)
 });
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
